46024e1a179474390c62ad39921d3cd2b0af5814
Stx69 <rudisek@seznam.cz>
2024-12-29 13:30:13 +0100

    without cors

--- src/app.ts
+++ src/app.ts
@@ -30,12 +30,12 @@ const limiter = rateLimit({
 
 app.use(limiter); // Apply to all routes
 app.use(helmet()); // Secure app with Helmet
-app.use(
-  cors({
-    origin: [process.env.ORIGIN_WEB_CLIENT || "http://localhost:3000"], // Replace with your frontend URL
-    methods: ["GET", "POST"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [process.env.ORIGIN_WEB_CLIENT || "http://localhost:3000"], // Replace with your frontend URL
+//     methods: ["GET", "POST"],
+//   })
+// );
 
 // Root endpoint
 app.get("/", (req, res) => {
