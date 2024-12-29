e1a66e78dff70c338103df12a298935f5dec7cd0
Stx69 <rudisek@seznam.cz>
2024-12-29 14:02:10 +0100

    a

--- src/routes/conversation.ts
+++ src/routes/conversation.ts
@@ -51,7 +51,6 @@ async function callOpenAI(
 // Start a conversation init character and user name
 router.post("/start", async (req: Request, res: Response) => {
   const { username, message, indexedHistory } = req.body;
-  console.log("req", req);
 
   // temporary delete old history api/conversation/start is used only once
   conversationHistory[username] = [];
